f2b4088f1172bcea4ffb68a75e5f1efd5740d9b2
Hosseinkaveh <k.pouria7@gmail.com>
2022-09-20 17:35:47 +0430

    Create Api and db

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+[ApiController]
+ [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+         private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+     
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>>  Get()
+        {
+            return await _context.AppUsers.ToListAsync();
+        }
+           [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.AppUsers.FindAsync(id);
+        }
+    }
+}
