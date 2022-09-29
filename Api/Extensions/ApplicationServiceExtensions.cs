3a7373ee948917e96cb3fb8082e746cccbf06361
Hosseinkaveh <k.pouria7@gmail.com>
2022-09-30 02:27:40 +0330

    Add Extension For Clean Startup

---
+++ Api/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data;
+using Api.Interfaces;
+using Api.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService
+        (this IServiceCollection services,IConfiguration config) 
+        {
+             services.AddScoped<ITokenService,TokenService>();
+
+            services.AddDbContext<DataContext>(option =>{
+                option.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+
+        }      
+    }
+}
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Api.Data;
+using Api.Extensions;
 using Api.Interfaces;
 using Api.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -33,28 +34,14 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ITokenService,TokenService>();
-
-            services.AddDbContext<DataContext>(option =>{
-                option.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
+           services.AddApplicationService(_config);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>{
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokenkey"])),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+           services.AddIdentityServices(_config);
 
         }
 
