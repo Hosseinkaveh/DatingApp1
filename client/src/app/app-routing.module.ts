a7169ed38d8e599ee5cf02c6d36f4067cd4a9114
Hosseinkaveh <k.pouria7@gmail.com>
2022-10-04 16:55:03 +0330

    Adding an Angular route guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
+  {
+    path:'',
+    runGuardsAndResolvers:'always',
+    canActivate:[AuthGuard],
+    children:[
+      {path:'home',component:HomeComponent,},
+    ]
+  },
+
   {path:'',component:LoginComponent},
-  {path:'home',component:HomeComponent},
   {path:'register',component:RegisterComponent},
-  {path:'**',component:HomeComponent,pathMatch:'full'},
+  {path:'**',component:LoginComponent},
 ];
 
 @NgModule({
