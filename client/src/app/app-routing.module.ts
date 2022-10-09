1a0b82e768fdf0fc7ccfbc060b156015592f982b
Hosseinkaveh <k.pouria7@gmail.com>
2022-10-09 13:49:35 +0330

    Add Server Error page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -21,6 +22,7 @@ const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'error',component:TestErrorsComponent},
   {path:'not-found',component:NotFoundComponent},
+  {path:'server-error',component:ServerErrorComponent},
   {path:'**',component:LoginComponent,pathMatch:'full'},
 ];
 
