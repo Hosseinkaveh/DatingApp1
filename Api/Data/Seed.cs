c3e5d16bf47b96496bd8d974f5af162b907d7bfb
Hosseinkaveh <k.pouria7@gmail.com>
2022-10-12 13:24:56 +0330

    Seed Data

---
+++ Api/Data/Seed.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if(await context.Users.AnyAsync()) return;
+
+            var Textfile = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = System.Text.Json.JsonSerializer.Deserialize<List<AppUser>>(Textfile);
+            if(users == null) return
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+
+               user.PasswordSalt = hmac.Key;
+               user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+
+               await context.Users.AddAsync(user);
+            }
+            await context.SaveChangesAsync();
+
+
+        }
+        
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,9 +13,25 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateAsyncScope();
+            var Service = scope.ServiceProvider;
+
+            try
+            {
+                var context = Service.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+            }
+            catch (System.Exception ex)
+            {
+                var logger = Service.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex,"An error ocuurred during migration");
+
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
